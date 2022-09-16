cc7987dab046d05ea09130fb483168c9e3876a0e
rationalsa <belaboxproject@gmail.com>
2022-09-16 18:21:29 +0100

    P3450: def boot order: move NVMe after eMMC because it delays booting up significantly
    Boot order is now external SD>USB>eMMC>NVMe>PXE>DHCP

--- include/configs/p3450-0000.h
+++ include/configs/p3450-0000.h
@@ -20,8 +20,8 @@
 #define BOOT_TARGET_DEVICES(func) \
 	func(MMC, mmc, 1) \
 	func(USB, usb, 0) \
-	func(NVME, nvme, 0) \
 	func(MMC, mmc, 0) \
+	func(NVME, nvme, 0) \
 	func(PXE, pxe, na) \
 	func(DHCP, dhcp, na)
 
